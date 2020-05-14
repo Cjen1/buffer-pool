caed0863cd6b32e5425ef4f3c7baf8e68a2a43c6
Cjen1 <cjj39@cam.ac.uk>
2020-05-14 18:06:35 +0100

    Ensure created buffer is correct size, add test.mli to detect unused functions

--- buffer_pool.ml
+++ buffer_pool.ml
@@ -7,16 +7,29 @@ module type S = sig
   val reset : t -> unit
 
   val len : t -> int
+
+  val sub : t -> int -> t
 end
 
-module Make(B : S ) = struct
+module type Buffer_pool = sig
+  type t
+  type buffer
+  type pooled_buffer
+  val create : t -> int -> pooled_buffer
+  val release : t -> pooled_buffer -> unit
+  val make : unit -> t
+  val get_buf: pooled_buffer -> buffer
+end 
+
+module Make(B : S ) : Buffer_pool with type buffer = B.t = struct
 
   module M = Map.Make(Int)
 
   type buffer = B.t
 
-  type t = {mutable available : B.t list M.t}
+  type pooled_buffer = {buf:buffer; underlying:buffer}
 
+  type t = {mutable available : buffer list M.t}
 
   let make () = {available = M.empty}
 
@@ -24,13 +37,7 @@ module Make(B : S ) = struct
     let base = log(2.) in
     fun x -> log x /. base
 
-  let find_or_default m i default = 
-    match M.find_opt m i with 
-    | Some v -> v
-    | None -> 
-      default
-
-  let pop_or_create m i f =
+  let pop m i f =
     match M.find_opt i m with
     | Some [] -> raise @@ Invalid_argument "Empty list in map"
     | Some [b] -> 
@@ -40,24 +47,24 @@ module Make(B : S ) = struct
       m', b
     | None -> m, f()
 
-  let push_or_create m i v = 
+  let push m i v = 
     match M.find_opt i m with
     | Some bs -> M.add i (v :: bs) m
     | None -> M.add i [v] m
 
-  let new_id =
-    let max = Int.of_float @@ 2. ** 30. -. 1. in
-    fun () -> Random.int max
-
   let create t size = 
     let e = size |> Int.to_float |> lg2 |> ceil |> Int.of_float in
     let default () = B.create @@ Int.of_float (2. ** (Float.of_int e)) in
-    let avail', b = pop_or_create t.available e default in
+    let avail', b = pop t.available e default in
     t.available <- avail';
-    b
+    {buf = B.sub b size; underlying = b}
 
   let release t buf = 
+    let buf = buf.underlying in
     B.reset buf;
     let id = buf |> B.len |> Int.to_float |> lg2 |> ceil |> Int.of_float in
-    t.available <- push_or_create t.available id buf
+    t.available <- push t.available id buf
+
+  let get_buf buf = 
+    buf.buf
 end 
--- test.ml
+++ test.ml
@@ -1,48 +1,41 @@
+module BP_buffer = struct 
+  include Cstruct
 
-module BP = Buffer_pool.Make(struct
-    include Cstruct
+  let reset t = Cstruct.memset t 0
+  let sub t = Cstruct.sub t 0
+end 
 
-    let reset t = Cstruct.memset t 0
-    end)
-
-let strings = 
-  [ "1"
-  ; "12"
-  ; "1234"
-  ; "123456"
-  ; "223456" ]
-
-let (==) a b = Cstruct.equal a b
+module BP = Buffer_pool.Make(BP_buffer) 
 
 (* Create two buffers editing one should not affect the other *)
 let independence l = 
   let bp = BP.make () in
   let a = BP.create bp l in
   let b = BP.create bp l in
-  Cstruct.memset a 5;
+  Cstruct.memset (BP.get_buf a) 5;
   Crowbar.check @@ not (a == b)
 
 (* Size of a buffer that you get out is always >= what you request*)
 let sizing l = 
   let bp = BP.make () in
   let a = BP.create bp l in
-  Crowbar.check @@ (Cstruct.len a >= l)
+  Crowbar.check @@ (Cstruct.len (BP.get_buf a) >= l)
 
 (* When a buffer is released it is zero'd *)
 let release l = 
   let bp = BP.make () in
   let buf = BP.create bp l in
-  Cstruct.memset buf 5;
+  Cstruct.memset (BP.get_buf buf) 5;
   BP.release bp buf;
   let buf' = BP.create bp l in
   Crowbar.check_eq 
     ~cmp:Cstruct.compare 
-    (Cstruct.create (Cstruct.len buf)) buf'
+    (Cstruct.create (Cstruct.len (BP.get_buf buf))) (BP.get_buf buf')
 
 let () =
   let open Crowbar in
-  add_test ~name:"independence" [int16] independence;
-  add_test ~name:"sizing" [int16] sizing;
-  add_test ~name:"release" [int16] release
+  add_test ~name:"independence" [uint16] independence;
+  add_test ~name:"sizing" [uint16] sizing;
+  add_test ~name:"release" [uint16] release
 
 
