52005b040c25aa0a824fbcb1d2ec1567ae10a65d
Chris Jensen <cjj39@cam.ac.uk>
2020-05-16 14:07:21 +0100

    Make compatible with Ocaml 4.03.0, api change
    API change should allow for better memory performance with the buffers

--- buffer_pool.ml
+++ buffer_pool.ml
@@ -7,7 +7,7 @@ module type Buffer = sig
 
   val len : t -> int
 
-  val sub : t -> int -> t
+  val sub : t -> int -> int -> t
 end
 
 module Make( B : Buffer ) : sig
@@ -15,13 +15,15 @@ module Make( B : Buffer ) : sig
   type pooled_buffer
   type buffer = B.t
   val get_buf: pooled_buffer -> buffer
-  val create : t -> int -> pooled_buffer
+  val alloc : t -> int -> pooled_buffer
+  val create : B.t -> pooled_buffer
   val release : t -> pooled_buffer -> unit
   val make : unit -> t
+  val sub : pooled_buffer -> int -> int -> pooled_buffer
 end 
   = struct
 
-  module M = Map.Make(Int)
+  module M = Map.Make(struct type t = int let compare = compare end)
 
   type buffer = B.t
 
@@ -35,8 +37,15 @@ end
     let base = log(2.) in
     fun x -> log x /. base
 
+  let find_opt i m = 
+  try
+    let res = M.find i m in
+    Some res
+  with Not_found -> 
+    None
+
   let pop m i f =
-    match M.find_opt i m with
+    match find_opt i m with
     | Some [] -> raise @@ Invalid_argument "Empty list in map"
     | Some [b] -> 
       M.remove i m, b
@@ -46,27 +55,30 @@ end
     | None -> m, f()
 
   let push m i v = 
-    match M.find_opt i m with
+    match find_opt i m with
     | Some bs -> M.add i (v :: bs) m
     | None -> M.add i [v] m
 
-  let create t size = 
-    let e = size |> Int.to_float |> lg2 |> ceil |> Int.of_float in
-    let default () = B.create @@ Int.of_float (2. ** (Float.of_int e)) in
+  let alloc t size = 
+    let e = size |> float_of_int |> lg2 |> ceil |> int_of_float in
+    let default () = B.create @@ int_of_float (2. ** (float_of_int e)) in
     let avail', b = pop t.available e default in
     t.available <- avail';
-    {buf = B.sub b size; underlying = b}
+    {buf = B.sub b 0 size; underlying = b}
+
+  let create buf = 
+    {buf; underlying=buf}
 
   let release t buf = 
     let buf = buf.underlying in
     B.reset buf;
-    let id = buf |> B.len |> Int.to_float |> lg2 |> ceil |> Int.of_float in
+    let id = buf |> B.len |> float_of_int |> lg2 |> ceil |> int_of_float in
     t.available <- push t.available id buf
 
   let get_buf buf = 
     buf.buf
 
   let sub buf off len = 
-    let buf' = B.sub buf.buf len in
+    let buf' = B.sub buf.buf off len in
     {buf with buf = buf'}
 end 
--- buffer_pool.mli
+++ buffer_pool.mli
@@ -8,7 +8,7 @@ module type Buffer = sig
 
   val len : t -> int
 
-  val sub : t -> int -> t
+  val sub : t -> int -> int -> t
 end
 
 module Make ( B : Buffer ) : sig
@@ -24,10 +24,18 @@ module Make ( B : Buffer ) : sig
   val get_buf : pooled_buffer -> buffer
   (** [get_buf b] gets the buffer represented by [b] *) 
 
-  val create : t -> int -> pooled_buffer
-  (** [create p len] create a new buffer from the pool [p] of length [len].
+  val sub : pooled_buffer -> int -> int -> pooled_buffer 
+  (** [sub buf off len] creates a sub-buffer starting at offset and len long.
+      If it goes out of bounds an error is thrown. *)
+
+  val alloc : t -> int -> pooled_buffer
+  (** [alloc p len] create a new buffer from the pool [p] of length [len].
       This may be a new buffer or it may be a one which was previously
       released. *)
+
+  val create : B.t -> pooled_buffer
+  (** [create buf] creates a new pooled of the buffer (This is copy free)
+    *)
 
   val release : t -> pooled_buffer -> unit
   (** [release p buf] releases [buf] use and adds it back to [p].
@@ -35,6 +43,7 @@ module Make ( B : Buffer ) : sig
 
   val make : unit -> t
   (** [make ()] creates a new buffer pool *)
+
 end 
 
 
